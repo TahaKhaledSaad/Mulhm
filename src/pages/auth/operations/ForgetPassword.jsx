d622b5d36b1af9b86264eab4671004b6e140b7b2
TahaKhaledSaad <tahakhsaad55@gmail.com>
2024-04-29 14:53:16 +0300

    auth && packages

--- src/pages/auth/operations/ForgetPassword.jsx
+++ src/pages/auth/operations/ForgetPassword.jsx
@@ -1,28 +1,36 @@
-import './style.css'
+import { Link } from "react-router-dom";
+import "./style.css";
 
 export default function ForgetPassword() {
-    return (
-        <div className='parent'>
-            <div className='parent-content' style={{
-                width: 'fit-content',
-                height: 'auto',
-                padding: '50px 30px'
-            }}>
-                <div className="main-info">
-                    <div className="form-data">
-                        <h2 className='text-center'>Forgot password?</h2>
-                        <p className='desc text-center'>No worries, we’ll send you reset instruction.</p>
-                        <form>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id='email' placeholder="Enter your email" />
-                            <button>Reset Password</button>
-                        </form>
-                        <p style={{ textAlign: 'center', fontSize: '14.22px' }}>
-                            <span>Back to login</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="parent">
+      <div
+        className="parent-content"
+        style={{
+          width: "fit-content",
+          height: "auto",
+          padding: "50px 30px",
+        }}
+      >
+        <div className="main-info">
+          <div className="form-data">
+            <h2 className="text-center">Forgot password?</h2>
+            <p className="desc text-center">
+              No worries, we’ll send you reset instruction.
+            </p>
+            <form>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="Enter your email" />
+              <button>Reset Password</button>
+            </form>
+            <p style={{ textAlign: "center", fontSize: "14.22px" }}>
+              <Link to="/login">
+                <span>Back to login</span>
+              </Link>
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
